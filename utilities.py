96fd74093bf400b40135b312f5d76b82c04e936f
BEDIRHAN UGUZ <e2172070@ceng.metu.edu.tr>
2019-11-07 23:16:27 +0300

    fix

--- utilities.py
+++ utilities.py
@@ -1,3 +1,11 @@
-'''
+"""
 Utility function definitions goes here.
-'''
+"""
+
+import datetime
+
+def dateformatter(timestamp):
+    """
+        formats timestamp like "dd:MM:YY HH:mm:ss"
+    """
+    return datetime.datetime.fromtimestamp(timestamp).strftime("%d-%m-%Y %H:%M:%S")
