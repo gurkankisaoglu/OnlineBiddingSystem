e05cae606b10325a2f18c1575c38fb6adb28bd86
BEDIRHAN UGUZ <e2172070@ceng.metu.edu.tr>
2020-01-06 17:51:41 +0300

    consumers ready

--- CiftlikBank/ciftlikbank/consumers.py
+++ CiftlikBank/ciftlikbank/consumers.py
@@ -3,12 +3,10 @@ import json
 
 class SockConsumer(WebsocketConsumer):
 
-  waiters = set()
+  waiters = {}
   
   def connect(self):
     print("connectt")
-    SockConsumer.waiters.add(self)
-    print([i for i in SockConsumer.waiters])
     self.accept()
   
   def disconnect(self,close_code):
@@ -16,7 +14,8 @@ class SockConsumer(WebsocketConsumer):
     pass
 
   def receive(self, text_data):
-    print(json.loads(text_data))
-    for i in SockConsumer.waiters:
-      i.send(text_data=json.dumps({"msg":"broadcasted"}))
+    msg = json.loads(text_data)
+    if(msg["op"]=="socket_open"):
+      SockConsumer.waiters[msg["user_id"]]=self
+    
     self.send(text_data=json.dumps({"msg":"msmsmsmsmmsms"}))
